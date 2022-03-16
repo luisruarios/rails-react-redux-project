10e3f9259cc33a290ec7d99f0f3408c3d28b6d0c
luisruarios <luis.rua@guarapo.dev>
2022-03-16 10:43:28 -0500

    final-commit

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -2,26 +2,55 @@ import React from "react"
 import PropTypes from "prop-types"
 import { createStructuredSelector } from "reselect"
 import { connect } from "react-redux"
+import { List } from "semantic-ui-react"
 
 
 const GET_THINGS_REQUEST = 'GET_THINGS_REQUEST'
+const GET_THINGS_SUCCESS = 'GET_THINGS_SUCCESS'
 
 function getThings() {
   console.log('getThings() Action!!')
+  return dispatch => {
+    dispatch({ type: GET_THINGS_REQUEST })
+    return fetch ('v1/things.json')
+    .then(response => response.json())
+    .then(json => dispatch(getThingsSuccess(json)))
+    .catch(error => console.log(error))
+  }
+}
+
+export function getThingsSuccess(json){
   return {
-    type: GET_THINGS_REQUEST
+    type: GET_THINGS_SUCCESS,
+    json
   }
 }
 
 
 class HelloWorld extends React.Component {
   render() {
+    const { things } = this.props;
+    const thingList = things.map((thing) => {
+     
+    return (
+      <List.Item>
+        <List.Icon name='arrow circle right' size='large'
+        verticalAlign="middle"/>
+        <List.Content>
+          <List.Header as='a'>{thing.name}{' '}</List.Header>
+          <List.Description as='a'>{thing.guid}</List.Description>
+          </List.Content> 
+      </List.Item>
+    )
+  })
     return (
       <React.Fragment>
-        <h1>Greeting: {this.props.greeting}</h1>
+        <p>Greeting: {this.props.greeting}</p>
         <button className="getThingsBtn" onClick={() => this.props.getThings()}>
           getThings
         </button>
+        <br />
+        <ul>{thingList}</ul>
       </React.Fragment>
     );
   }
--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,18 +1,31 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const initialState = {
-    things: []
+    things: [
+        {
+            name: "test",
+            guid: "123"
+        }
+    ]
 }
 
-function rootReducer(state, action){
+function rootReducer(state, action) {
     console.log(action.type)
     switch (action.type) {
-        default:
-            return state;
+        case "GET_THINGS_SUCCESS":
+        return { things: action.json.things}
     }
+    return state
 }
 
-export default function configureStore(){
-    const store = createStore(rootReducer, initialState)
+export default function configureStore() {
+    const store = createStore(
+        rootReducer, 
+        initialState,
+        composeEnhancers(applyMiddleware(thunk))
+        
+        )
     return store
 }
